7def461c6658c0e339f256f941dc47db9a79699f
QueenieeQ <xuanquyhnvn@gmail.com>
2023-07-04 11:38:48 +0700

    update file path

--- maskDetector_data_preparation.py
+++ maskDetector_data_preparation.py
@@ -1,19 +1,19 @@
 from pathlib import Path
 
 import pandas as pd
-from google_drive_downloader import GoogleDriveDownloader as gdd
+# from google_drive_downloader import GoogleDriveDownloader as gdd
 from tqdm import tqdm
 
 # download dataset from link provided by
 # https://github.com/X-zhangyang/Real-World-Masked-Face-Dataset
-datasetPath = Path('covid-mask-detector/data/mask.zip')
-gdd.download_file_from_google_drive(file_id='1UlOk6EtiaXTHylRUx2mySgvJX9ycoeBp',
-                                    dest_path=str(datasetPath),
-                                    unzip=True)
+# datasetPath = Path('covid-mask-detector/data/mask.zip')
+# gdd.download_file_from_google_drive(file_id='1UlOk6EtiaXTHylRUx2mySgvJX9ycoeBp',
+#                                     dest_path=str(datasetPath),
+#                                     unzip=True)
 # delete zip file
-datasetPath.unlink()
+# datasetPath.unlink()
 
-datasetPath = Path('covid-mask-detector/data/self-built-masked-face-recognition-dataset')
+datasetPath = Path('/mnt/ssd_ext/phongphu/nxq/self-built-masked-face-recognition-dataset')
 maskPath = datasetPath/'AFDB_masked_face_dataset'
 nonMaskPath = datasetPath/'AFDB_face_dataset'
 maskDF = pd.DataFrame()
